61d3690d3b4fc3a291d3d58f5618610c0d13cadc
Petro Hubenya <p.hubenya@gmail.com>
2023-04-18 17:37:41 +0200

    Update Currency class according to /search/trending GET request.

--- CoinFactory/Models/Currency.cs
+++ CoinFactory/Models/Currency.cs
@@ -2,24 +2,58 @@
 {
 	public class Currency
 	{
-		public string CurrencyCode { get; set; }
-		public string CurrencyName { get; set; }
-		public double CurrencyPrice { get; set; }
-		public string CurrencyVolume { get; set; }
-		public string CurrencyWebPage { get; set; }
+		/* Below properties correspond to "/search/trending".
+		 * Get trending search coins (Top-7) on CoinGecko in the last 24 hours.*/
 
-		public Currency(string currencyCode, 
-						string currencyName,
-						double currencyPrice,
-						string currencyVolume,
-						string currencyWebPage)
+		public string Id { get; set; }
+		public int CoinId { get; set; }
+		public string Name { get; set; }
+		public string Symbol { get; set; }
+		public int MarketCapRank { get; set; }
+
+		// The smallest logo link.
+		public string Thumb { get; set; }
+
+		// The small logo link.
+		public string Small { get; set; }
+
+		// The large logo link.
+		public string Large { get; set; }
+
+		public string Slug { get; set; }
+		
+		// Price of the coin in Bitcoins.
+		public double PriceBtc { get; set; }
+		public int Score { get; set; }
+		
+
+		// Constructor.
+
+		public Currency(string id,
+						int coinId,
+						string name,
+						string symbol,
+						int marketCapRank,
+						string thumb,
+						string small,
+						string large,
+						string slug,
+						double priceBtc,
+						int score)
 		{
-			CurrencyCode = currencyCode;
-			CurrencyName = currencyName;
-			CurrencyPrice = currencyPrice;
-			CurrencyVolume = currencyVolume;
-			CurrencyWebPage = currencyWebPage;
+			Id = id;
+			CoinId = coinId;
+			Name = name;
+			Symbol = symbol;
+			MarketCapRank = marketCapRank;
+			Thumb = thumb;
+			Small = small;
+			Large = large;
+			Slug = slug;
+			PriceBtc = priceBtc;
+			Score = score;			
 		}
+
 
 	}
 }
--- CoinFactory/Models/Environment.cs
+++ CoinFactory/Models/Environment.cs
@@ -2,6 +2,35 @@
 {
 	public class Environment
 	{
+		// Write the property taht will contain API URL.
+		public string ApiUrl { get; set; }
+
+		// Write the constructor to create an instance with the property, containing API URL.
+		public Environment(string apiUrl)
+		{
+			ApiUrl = apiUrl;
+		}
+
+		// Write the method, that reads the API_URL.tsv file and gives it's value to property of Environment instance.
+		public static Environment FromTsv(string tsvLine)
+		{
+			/* This code opens the file in read mode and reads a single line from it using the ReadLine() method.
+			 * The line is then stored in the line variable as a string.*/
+
+			string line;
+			
+			using (StreamReader file = new StreamReader("API_URL.tsv"))
+			{
+				line = file.ReadLine();
+			}
+
+			// Create and return an inctance of the Environment class with the property containing API URL.
+
+			Environment environment = new Environment(line);
+
+			return environment;
+		}
+
 
 	}
 }
